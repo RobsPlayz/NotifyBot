1c4ae74539341ed5ad3343c67dba0e5801dd9e4b
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-17 23:06:10 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ bot = commands.Bot(command_prefix="n!", description="help")
 @bot.event
 async def on_ready():
   print('Im ready! Bot by Drifty!')
-  @bot.command()
+@bot.command()
 async def credits(ctx, *tells who created it):
     await ctx.send('By Drifty')
-    @bot.command()
+@bot.command()
 async def invite(ctx, *invites the bot):
     await ctx.send('https://discordapp.com/oauth2/authorize?&client_id=513156006973538313&scope=bot&permissions=268436512')
 @bot.command()
