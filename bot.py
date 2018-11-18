ca32dcaa5191c59f116e27b29beb867f924bfb5a
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-18 00:33:11 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,6 @@ logging.basicConfig(level='INFO')
 
 bot = commands.Bot(command_prefix="n!", description="help")
 
-@commands.command(name="customstatus", aliases=['status', 'presence'])
-    @commands.has_permissions(administrator=True)
-    async def _status(self, ctx, *, message):
-        '''Set a custom playing status for the bot.'''
-        if message == 'clear':
-            return await self.change_presence(activity=None)
-        await self.change_presence(activity=discord.Game(message))
-        await ctx.send(f"Changed status to **{message}**")
 @bot.command()
 async def credits(ctx,):
     await ctx.send('By Drifty')
