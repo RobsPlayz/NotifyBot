0179504250117478551b98e6b441e927b7699406
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-18 20:27:03 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,17 @@ newUserMessage = "A New user has joined!" # customise this to the message you wa
 
 
 bot = commands.Bot(command_prefix="n!", description="help")
-
+@bot.command(pass_context = True)
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+        role = discord.utils.get(member.server.roles, name='Muted')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)
+        
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member):
