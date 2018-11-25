256b771220ec6e202d455b589e535d24fc49de86
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-25 22:21:47 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,23 +5,56 @@ import os
 import logging
 import asyncio
 import random
+import sys, traceback
 
 client=discord.Client()
 
-@client.event
-async def on_ready():
-    print('logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('-----')
+def get_prefix(bot, message):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
+    # Notice how you can use spaces in prefixes. Try to keep them simple though.
+    prefixes = ['>?', '! ', 'n!']
+
+    # Check to see if we are outside of a guild. e.g DM's etc.
+    if not message.guild:
+        # Only allow ? to be used in DMs
+        return '?'
+
+    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
+# Think of it like a dot path import
+initial_extensions = ['cogs.simple',
+                      'cogs.members',
+                      'cogs.owner']
+
+bot = commands.Bot(command_prefix=get_prefix, description='help')
+
+# Here we load our extensions(cogs) listed above in [initial_extensions].
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+
+@bot.event
+async def on_ready():
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+    # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
+    await bot.change_presence(game=discord.Game(name='Cogs Example', type=1, url='https://twitch.tv/kraken'))
+    print(f'Successfully logged in and booted...!')
 
 logging.basicConfig(level='INFO')
 
 newUserMessage = "A New user has joined!" # customise this to the message you want to send new users
-
-
-bot = commands.Bot(command_prefix="n!", description="help")
 
 @bot.command()
 async def mute(ctx,):
