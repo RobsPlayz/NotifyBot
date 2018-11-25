293a3388b45495c0a78aaecd475f7eb1853f3332
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-25 22:59:56 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
     # Notice how you can use spaces in prefixes. Try to keep them simple though.
-    prefixes = ['noti!', 'notify! ', 'n!']
+    prefixes = ['noti!', 'noty!', 'n!']
 
     # Check to see if we are outside of a guild. e.g DM's etc.
     if not message.guild:
