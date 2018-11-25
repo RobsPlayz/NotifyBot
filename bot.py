947131d8fd5b6dcb24565e46d4be626dede308c3
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-25 22:54:42 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,18 +14,18 @@ class MembersCog:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+@commands.command()
     @commands.guild_only()
     async def joined(self, ctx, *, member: discord.Member):
         """Says when a member joined."""
         await ctx.send(f'{member.display_name} joined on {member.joined_at}')
 
-    @commands.command(name='coolbot')
+@commands.command(name='coolbot')
     async def cool_bot(self, ctx):
         """Is the bot cool?"""
         await ctx.send('This bot is cool. :)')
 
-    @commands.command(name='top_role', aliases=['toprole'])
+@commands.command(name='top_role', aliases=['toprole'])
     @commands.guild_only()
     async def show_toprole(self, ctx, *, member: discord.Member=None):
         """Simple command which shows the members Top Role."""
@@ -40,7 +40,6 @@ class MembersCog:
     async def check_permissions(self, ctx, *, member: discord.Member=None):
         """A simple command which checks a members Guild Permissions.
         If member is not provided, the author will be checked."""
-
         if not member:
             member = ctx.author
 
@@ -56,12 +55,61 @@ class MembersCog:
 
         await ctx.send(content=None, embed=embed)
         # Thanks to Gio for the Command.
+##crash2?
 
+class OwnerCog:
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # Hidden means it won't show up on the default help.
+    @commands.command(name='load', hidden=True)
+    @commands.is_owner()
+    async def cog_load(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def cog_unload(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def cog_reload(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+
+def setup(bot):
+    bot.add_cog(OwnerCog(bot))
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
     bot.add_cog(MembersCog(bot))
- cogs.owner.py
 from discord.ext import commands
 
 
